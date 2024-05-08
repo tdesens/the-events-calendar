25fc180971ef031d786aa5087f952d18a75be2b8
Stephen Page <stephen@theeventscalendar.com>
2024-05-08 10:22:26 -0400

    Revert "Stop hiding the template selector."

--- src/Events/Integrations/Plugins/Elementor/Template/Controller.php
+++ src/Events/Integrations/Plugins/Elementor/Template/Controller.php
@@ -91,10 +91,12 @@ class Controller extends Controller_Contract {
 	 * @since 6.4.0
 	 */
 	public function add_filters(): void {
+		add_filter( 'tec_events_should_display_events_template_setting', '__return_false' );
 		add_filter( 'tribe_events_template_single-tribe_events.php', [ $this, 'filter_override_event_template' ] );
 		add_filter( 'tribe_events_template_single-event.php', [ $this, 'filter_override_event_template' ] );
 		add_filter( 'tribe_get_option_tribeEventsTemplate', [ $this, 'filter_events_template_setting_option' ] );
 		add_filter( 'tribe_get_single_option', [ $this, 'filter_tribe_get_single_option' ], 10, 3 );
+		add_filter( 'tribe_settings_save_option_array', [ $this, 'filter_tribe_save_template_option' ], 10, 2 );
 	}
 
 	/**
@@ -107,38 +109,20 @@ class Controller extends Controller_Contract {
 		remove_filter( 'tribe_events_template_single-event.php', [ $this, 'filter_override_event_template' ] );
 		remove_filter( 'tribe_get_option_tribeEventsTemplate', [ $this, 'filter_events_template_setting_option' ] );
 		remove_filter( 'tribe_get_single_option', [ $this, 'filter_tribe_get_single_option' ], 10 );
+		remove_filter( 'tribe_settings_save_option_array', [ $this, 'filter_tribe_save_template_option' ], 10 );
 	}
 
 	/**
-	 * Force the correct Single Event template object for Elementor theme.
+	 * Force the correct template object for Elementor theme.
 	 *
 	 * @since 6.4.0
 	 *
-	 * @param string $option The value of the option.
+	 * @param string $value The value of the option.
 	 *
-	 * @return string $option The original value, or an empty string signifying the default events template.
+	 * @return string $value The original value, or an empty string if FSE is active.
 	 */
-	public function filter_events_template_setting_option( $option ): string {
-		// Only for events.
-		if ( ! is_singular( TEC::POSTTYPE ) ) {
-			return $option;
-		}
-
-		// Only for single events.
-		if ( ! tribe( Template_Bootstrap::class )->is_single_event() ) {
-			return $option;
-		}
-
-		/**
-		 * Allows filtering of the events template setting option override.
-		 *
-		 * @since TBD
-		 *
-		 * @param string $value  The value of the option. If this is set to anything other than an empty string,
-		 *                       it will prevent the use of the provided Elementor templates.
-		 * @param string $option The original value, or an empty string signifying the default events template.
-		 */
-		return apply_filters( 'tec_events_filter_events_template_setting_option', '', $option );
+	public function filter_events_template_setting_option( $value ): string {
+		return '';
 	}
 
 	/**
@@ -146,38 +130,39 @@ class Controller extends Controller_Contract {
 	 *
 	 * @since 5.14.2
 	 *
-	 * @param mixed  $option        Results (value) of option query.
+	 * @param mixed  $option        Results of option query.
 	 * @param string $default_value The default value.
 	 * @param string $option_name   Name of the option.
 	 *
 	 * @return mixed results of option query.
 	 */
 	public function filter_tribe_get_single_option( $option, $default_value, $option_name ) {
-		// ONly this option.
 		if ( 'tribeEventsTemplate' !== $option_name ) {
 			return $option;
 		}
 
-		// Only for events.
-		if ( ! is_singular( TEC::POSTTYPE ) ) {
-			return $option;
+		return '';
+	}
+
+	/**
+	 * Overwrite the template option on save if Elementor is active.
+	 * We only support the default events template for now.
+	 *
+	 * @since 6.4.0
+	 *
+	 * @param array<string, mixed> $options   The array of values to save. In the format option key => value.
+	 * @param string               $option_id The main option ID.
+	 *
+	 * @return array<string, mixed> $options   The array of values to save. In the format option key => value.
+	 */
+	public function filter_tribe_save_template_option( $options, $option_id ): array {
+		if ( ! is_array( $options ) ) {
+			$options = [];
 		}
 
-		// Only for single events.
-		if ( ! tribe( Template_Bootstrap::class )->is_single_event() ) {
-			return $option;
-		}
+		$options['tribeEventsTemplate'] = '';
 
-		/**
-		 * Allows filtering of the events single template option override.
-		 *
-		 * @since TBD
-		 *
-		 * @param string $value  The value of the `tribeEventsTemplate` option. If this is set to anything other
-		 *                       than an empty string, it will prevent the use of the provided Elementor templates.
-		 * @param string $option The original value, or an empty string signifying the default events template.
-		 */
-		return apply_filters( 'tec_events_filter_tribe_get_single_option', '', $option );
+		return $options;
 	}
 
 	/**
